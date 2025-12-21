610492b04cef8ebf99e51f480b7ad22280a37da7
ASMA <asmayahya300@gmail.com>
2025-12-21 19:48:19 +0200

    Update Todo App features

--- java.js
+++ java.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const taskInput = document.getElementById("taskInput");
   const addTaskButton = document.getElementById("addTaskButton");
+  const todoList = document.getElementById("todoList");
+  const deleteDoneTasks = document.getElementById("deleteDoneTasks");
+  const deleteAllTasks = document.getElementById("deleteAllTasks");
+  const allTasksButton = document.getElementById("allTasks");
+  const doneTasksButton = document.getElementById("doneTasks");
+  const todoTasksButton = document.getElementById("todoTasks");
+  const confirmationModal = document.getElementById("confirmationPopup");
+  const confirmDeleteButton = document.getElementById("confirmDeleteButton");
+  const cancelDeleteButton = document.getElementById("cancelDeleteButton");
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
